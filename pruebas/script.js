9324aaf66e517222b6240f6f436c36c6683f1f1e
adaatidev <ada.atienza@iesnervion.es>
2026-03-24 09:19:37 +0100

    JS

---
+++ pruebas/script.js
@@ -0,0 +1,9 @@
+const input1 = document.getElementById("inputNum1");
+const input2 = document.getElementById("inputNum2");
+const calcular = document.getElementById("btnCalcular");
+const resultado = document.getElementById("mensaje");
+
+function obtenerValores() {
+const n1 = parseInt(input1.value);
+const n2 = parseInt(input2.value);
+}
